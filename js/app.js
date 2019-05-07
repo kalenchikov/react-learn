c367096352949312b7f8d721e87c44d923a52bac
kalenchikov <kalenchikov@gmail.com>
2019-05-07 17:20:30 +0600

    2 step

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
-ReactDOM.render(React.createElement('h1', null, 'Hi, World!'),
+class News extends React.Component {
+	render() {
+		return (
+			<div className="news">
+				К сожалению. новостей нет.
+			</div>
+		)
+	}
+}
+
+class Comments extends React.Component {
+	render() {
+		return (
+			<div className="comments">
+				Нет новостей - комментировать нечего.
+			</div>
+		)
+	}
+}
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="app">
+				Всем привет, я компонент App! Я умею отображать новости.
+				<News />
+				<Comments />
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(
+	<App />,
 	document.getElementById('root')
 )
